83c4724cf68b90f9b0e52be9682a6862d59738a3
E1070 <ethan.wang@ecoflow.com>
2025-03-21 17:40:17 +0800

    feat(gateway & oms): 新增网关应用及OMS服务相关模块和配置

---
+++ oneflow-gateway/src/main/java/com/oneflow/gateway/GatWayApplication.java
@@ -0,0 +1,8 @@
+package com.oneflow.gateway;
+
+@SpringBootApplication
+public class GatWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatWayApplication.class, args);
+    }
+}
---
+++ oneflow-oms/oms-service/src/main/java/com/oneflow/oms/OmsApplication.java
@@ -0,0 +1,8 @@
+package com.oneflow.oms;
+
+@SpringBootApplication
+public class OmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OmsApplication.class, args);
+    }
+}
